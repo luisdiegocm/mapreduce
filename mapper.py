82b3380f6384fdc6de7842f858097a708947d2ec
luisdiegocm <diego_conejo@seneca3.lehre.hwr-berlin.de>
2019-05-12 18:46:51 +0200

    3rd Result

--- mapper.py
+++ mapper.py
@@ -34,6 +34,9 @@ for line in sys.stdin:
     # the result is a tuple with 6 elements
     data = line.strip().split("\t")
 
+    if (len(data) != 6):
+    	raise Exception("Error: there are not 6 elements in the tuple")  
+
     # store the 6 elements of the tuple in seperate variables
     date, time, item, category, sales, payment = data
 
